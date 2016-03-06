a7cab591d3fc8f6e3d24d17b48fb07e8ba7f0849
jenb <jjbordafunsanmateo.edu.co>
2016-03-06 02:10:54 -0500

    se creo la variable para poder ser validada con el servlet y se realizo la primera prueba

---
+++ src/java/Servletconjuntocantor.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author SALA6
+ */
+@WebServlet(urlPatterns = {"/Servlet"})
+public class Servletconjuntocantor extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String numero1=request.getParameter("numero");
+            double numero2=Double.parseDouble(numero1);
+            if(numero2>=0.333 &numero2<=0.666)
+                out.println("<h1>tu numero esta en la primera iteracion "  + "</h1>");
+            else
+//                if(numero2>=0 &numero2<=1)
+                out.println("<h1>tu numero esta fuera de rango "  + "</h1>");
+            /* TODO output your page here. You may use following sample code. */
+           
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ejemplo</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+                    
+            out.println("<h1>tu numero es : " + request.getParameter("numero") + "</h1>");
+            
+            
+          
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
